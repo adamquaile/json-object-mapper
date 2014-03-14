1b81eaeb00c2a8b8788ed4f32c1d4eda189b816a
Adam Quaile <adamquaile@gmail.com>
2014-03-14 21:41:38 +0000

    Tidied EntityManager

--- src/AdamQuaile/JsonObjectMapper/EntityManager.php
+++ src/AdamQuaile/JsonObjectMapper/EntityManager.php
@@ -10,7 +10,21 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
  * Class to handle searching, retrieving and mapping objects
  * from JSON files.
  *
- * See docs/02-Querying.md for usage
+ * See docs/02-Querying.md for usage.
+ *
+ *
+ * A note on stubs / full objects:
+ *
+ * When searching for an object, in order to avoid issues with
+ * cyclic associations and infinite recursion, all objects are
+ * created as stubs. Each association followed will get another
+ * stub object.
+ *
+ * The code then iterates through each stub and replaces associations
+ * with reference to the new object. It is then considered complete.
+ *
+ * At the end of this process, all the objects are full, with
+ * cyclic relationships.
  *
  * @author Adam Quaile <adamquaile@gmail.com>
  */
@@ -33,78 +47,27 @@ class EntityManager
     private $fullObjects = [];
 
     /**
-     * @var string[]
-     */
-    private $incompleteStubIds = [];
-
-    /**
      * @var \Symfony\Component\PropertyAccess\PropertyAccessor
      */
     private $accessor;
 
+    /**
+     * Create an entity manager for files in a certain directory
+     *
+     * @param $location
+     */
     public function __construct($location)
     {
         $this->location = realpath($location);
         $this->accessor = PropertyAccess::createPropertyAccessor();
     }
 
-    private function getFileContents($id)
-    {
-        return file_get_contents($this->getFilename($id));
-    }
-
-
-
-    public function find($id)
-    {
-        if (!isset($this->fullObjects[$id])) {
-            $stub = $this->_find($id);
-
-            // Go through stubs until empty..
-            while (count($this->stubObjects) > 0) {
-
-                // Get the first
-                $keys = array_keys($this->stubObjects);
-                $stub = $this->stubObjects[$keys[0]];
-
-                $this->replaceAssociationsWithObjects($stub);
-                $this->markComplete($keys[0], $stub);
-            }
-        }
-        return $this->fullObjects[$id];
-    }
-
-    private function replaceAssociationsWithObjects($stub)
-    {
-        foreach ($this->getPropertiesForId($this->accessor->getValue($stub, 'id')) as $property) {
-
-            $parsedValue = $this->parseAssociations($this->accessor->getValue($stub, $property));
-            $this->accessor->setValue($stub, $property, $parsedValue);
-        }
-    }
-    private function getPropertiesForId($id)
-    {
-        return array_keys($this->arrayFromJsonFile($this->getFilename($id)));
-    }
-
-    private function markComplete($id, $object)
-    {
-        unset($this->stubObjects[$id]);
-        $this->fullObjects[$id] = $object;
-    }
-
-    private function _find($id)
-    {
-        if (isset($this->fullObjects[$id])) {
-            return $this->fullObjects[$id];
-        }
-        if (isset($this->stubObjects[$id])) {
-            return $this->stubObjects[$id];
-        }
-
-        return $this->stubObjects[$id] = $this->getStubFromId($id);
-    }
-
+    /**
+     * Find all objects within a directory / namespace
+     *
+     * @param $namespace
+     * @return array
+     */
     public function findAll($namespace)
     {
         $finder = new Finder();
@@ -122,6 +85,80 @@ class EntityManager
         return $entities;
     }
 
+    /**
+     * Find a single object by its ID.
+     *
+     * @param $id
+     * @return object
+     */
+    public function find($id)
+    {
+        // Return if we already have it
+        if (!isset($this->fullObjects[$id])) {
+
+            $this->_find($id);
+
+            // Go through stubs until empty..
+            while (count($this->stubObjects) > 0) {
+
+                // Get the first
+                $stubID = array_keys($this->stubObjects)[0];
+                $stub = $this->stubObjects[$stubID];
+
+                // Iterate, linking associations..
+                $this->replaceAssociationsWithObjects($stub);
+
+                // ... and marking as full object
+                unset($this->stubObjects[$stubID]);
+                $this->fullObjects[$stubID] = $stub;
+            }
+        }
+        return $this->fullObjects[$id];
+    }
+
+    /**
+     * Iterate through each of the properties loaded from JSON file
+     * and replace any relevant values with the objects they
+     * should be associated with (either stub or full)
+     *
+     * @param $stub
+     */
+    private function replaceAssociationsWithObjects($stub)
+    {
+        $id = $this->accessor->getValue($stub, 'id');
+        $stubProperties = array_keys($this->arrayFromJsonFile($this->getFilename($id)));
+
+        foreach ($stubProperties as $property) {
+
+            $parsedValue = $this->parseAssociations($this->accessor->getValue($stub, $property));
+            $this->accessor->setValue($stub, $property, $parsedValue);
+        }
+    }
+
+    /**
+     * Used internally, either the stub object or the full one
+     * if we have it.
+     *
+     * @param $id
+     * @return object
+     */
+    private function _find($id)
+    {
+        if (isset($this->fullObjects[$id])) {
+            return $this->fullObjects[$id];
+        }
+        if (isset($this->stubObjects[$id])) {
+            return $this->stubObjects[$id];
+        }
+
+        return $this->stubObjects[$id] = $this->getStubFromId($id);
+    }
+
+    /**
+     * Load a stub object from an ID
+     * @param $id
+     * @return object
+     */
     private function getStubFromId($id)
     {
         $data = $this->arrayFromJsonFile($this->getFilename($id));
@@ -136,6 +173,35 @@ class EntityManager
     }
 
     /**
+     * Create an object of the given class and set its properties.
+     *
+     * Uses Reflection and Symfony's PropertyAccess component
+     *
+     * @param $className    string
+     * @param $data         array
+     *
+     * @return object
+     */
+    public function hydrateObject($className, $data)
+    {
+
+        $reflectionClass = new \ReflectionClass($className);
+        $object = $reflectionClass->newInstanceWithoutConstructor();
+
+        foreach ($data as $key => $value) {
+            try {
+
+                $this->accessor->setValue($object, $key, $value);
+            } catch (NoSuchPropertyException $e) {
+                $object->$key = $value;
+            }
+        }
+
+        return $object;
+    }
+
+    /**
+     * Get associative array of the JSON file
      * @param $filename
      * @return array
      */
@@ -144,6 +210,15 @@ class EntityManager
         return json_decode(file_get_contents($filename), true);
     }
 
+    /**
+     * Get a list of folders between the base location and the object's
+     * directory where potential meta-files may reside.
+     *
+     * Returns upper-level folders first
+     *
+     * @param $id
+     * @return array
+     */
     private function getFoldersInHierarchy($id)
     {
         $folders = [$this->location];
@@ -156,6 +231,13 @@ class EntityManager
         return $folders;
     }
 
+    /**
+     * Get fully merged metadata (currently only class to hydrate)
+     * for an object
+     *
+     * @param $id
+     * @return array
+     */
     private function getMetadataForEntity($id)
     {
 
@@ -184,26 +266,12 @@ class EntityManager
         return $currentMetaData;
     }
 
-    public function hydrateObject($className, $data)
-    {
-
-        $reflectionClass = new \ReflectionClass($className);
-        $object = $reflectionClass->newInstanceWithoutConstructor();
-
-        foreach ($data as $key => $value) {
-            try {
-
-                $this->accessor->setValue($object, $key, $value);
-            } catch (NoSuchPropertyException $e) {
-                $object->$key = $value;
-            }
-        }
-
-        return $object;
-
-
-    }
-
+    /**
+     * Replace any matching values with their associations
+     *
+     * @param $value
+     * @return array|object
+     */
     private function parseAssociations($value)
     {
         switch (true) {
@@ -217,6 +285,12 @@ class EntityManager
         }
     }
 
+    /**
+     * Does a scalar string look like an association reference?
+     *
+     * @param $value string
+     * @return bool|string
+     */
     private function isAssociationRef($value)
     {
         return (mb_strlen($value) > 1) && ('@' === mb_substr($value, 0, 1))
@@ -226,6 +300,8 @@ class EntityManager
     }
 
     /**
+     * Get the full filename for an ID
+     *
      * @param $id
      * @return string
      */
